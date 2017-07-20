0298b4aeb5c23e1703e0722ccb4e66eeb9082f67
Maya Levi <maya.isabellevi@gmail.com>
2017-07-20 15:15:25 -0700

    update views and url.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Create your views heredef post_list(request):
+
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
